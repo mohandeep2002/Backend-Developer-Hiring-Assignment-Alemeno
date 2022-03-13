f54ec02773c6b80022680c4652b31ab8d7398186
mohandeep2002 <mohandeep2002@gmail.com>
2022-03-13 19:39:47 +0530

    HTML pages

--- kids/urls.py
+++ kids/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, add_kids, add_images
 urlpatterns = [
     path('', home, name="home"),
+    path('add_kids/', add_kids, name="add_kids"),
+    path('add_images/', add_images, name="add_images")
 ]
--- kids/views.py
+++ kids/views.py
@@ -2,6 +2,16 @@ from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def add_kids(request):
+    #return HttpResponse('add kids page')
+    return render(request, "Add_Kids.html")
+
+def add_images(request):
+    #return HttpResponse('add images page')
+    return render(request, "Add_Images.html")
