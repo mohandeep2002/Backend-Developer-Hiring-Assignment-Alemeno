ecad3052635a8070993ec9c560d664e19cc9f1cb
mohandeep2002 <mohandeep2002@gmail.com>
2022-03-13 19:03:20 +0530

    Added Kids Model

--- alemeno/settings.py
+++ alemeno/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kids',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
--- kids/models.py
+++ kids/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
+
 # Create your models here.
+class Kid(models.Model):
+    kid_id = models.AutoField(primary_key=True)
+    kid_name = models.CharField(max_length=30)
+    kid_age = models.IntegerField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=False)
+    parents_email_id = models.EmailField()
