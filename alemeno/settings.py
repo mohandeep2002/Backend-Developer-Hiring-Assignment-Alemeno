d18cdce623245d851d7a59fcb7a009f34aa3c9ca
mohandeep2002 <mohandeep2002@gmail.com>
2022-03-15 23:32:24 +0530

    mail sent successfully

--- alemeno/settings.py
+++ alemeno/settings.py
@@ -127,3 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mohandeep2002@gmail.com'
+EMAIL_HOST_PASSWORD = 'xsfpjtwcdxvlfjgs'
--- kids/views.py
+++ kids/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Kid
 from .forms import ImageForm, KidsForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Create your views here.
@@ -30,8 +32,17 @@ def add_images(request):
         form = ImageForm(request.POST, request.FILES)
         print(form.is_valid())
         if form.is_valid():
-            form.save()
+            f = form.save()
             print(form.errors)
+            if f.food_group == "Unknown":
+                print("yes", f.image_id.parents_email_id)
+                destination = str(f.image_id.parents_email_id)
+                subject = "Unknown Category in Food Group"
+                message = "Unknown Category in Food Group \nName:" + str(f.image_id.kid_name) + "\nParents Ph no" + str(
+                    f.image_id.phone_number)
+                source = settings.EMAIL_HOST_USER
+                send_mail(subject, message, source, [destination,])
+                print("mail sent")
             return render(request, "Add_Images.html", {"msg": 'Response Saved'})
         else:
             print(form.errors)
